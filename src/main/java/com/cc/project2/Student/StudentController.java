f374b8f3c3bf1c62b43f8a2a6de5066f88ce1f50
aditipanigrahi <adpan2009@gmail.com>
2019-04-30 17:31:14 -0700

    Changes to api's after testing

--- src/main/java/com/cc/project2/Student/StudentController.java
+++ src/main/java/com/cc/project2/Student/StudentController.java
@@ -34,50 +34,54 @@ public class StudentController {
 
 	@PostMapping("/generateStudentHomePage")
 	public List<AccomodationStatus> studentLogin(@RequestBody UserAccomodationParentObject userAccomodationParentObject) {
+		System.out.println("Matched list size 1");
 
 		List<AccomodationStatus> hostListings  = new ArrayList<AccomodationStatus>();
 		if(userAccomodationParentObject!=null) {
+			System.out.println("Matched list size 2");
+
 			User user = userAccomodationParentObject.getUser();
 			if(user==null)
 				return null;
 			//	Accomodation accomodation = userAccomodationParentObject.getAccomodation();
-			if(user.getUserType().toLowerCase()=="student") {
-				String username = user.getUserName();
-				String password = user.getPassword();
-				List<User> userList=UserRepository.findByUserNameAndPassword(username, password);
-				if(userList==null && userList.get(0)==null) {
-					return null;
-				}
-				Long userId = userList.get(0).getUserId();
-				List<Accomodation> accomodationList = AccomodationRepository.findByUsername(username);
-				if(accomodationList==null || accomodationList.get(0)==null) {
-					return null;
-				}
-				Accomodation accomodation = accomodationList.get(0);
-				List<Accomodation> accomodationListMatched = new ArrayList<Accomodation>();
-				if(accomodation != null && accomodation.getUserType()!=null 
-						&& accomodation.getRoommatePreference()!=null && accomodation.getStartDate()!=null) {
-					accomodationListMatched = AccomodationRepository.findByUserTypeAndRoommatePreferenceAndStartDate
-							(accomodation.getUserType(), accomodation.getRoommatePreference(), 
-									accomodation.getStartDate());
-				}
-				for(Accomodation matchedAc: accomodationListMatched) {
-					User student = UserRepository.findByUserName(username);
-					List<StudentHostMatch> match = studentHostMatchRepository.findByUserId(student.getUserId());
-					AccomodationStatus accomodationStatus = new AccomodationStatus();
-					for(StudentHostMatch studentHostMatch: match) {
-						if(matchedAc.getAcId()==studentHostMatch.getAcId()) {
-							accomodationStatus.setAccomodation(matchedAc);
-							accomodationStatus.setStatus(studentHostMatch.getStatus());
-						}
-						else {
-							accomodationStatus.setAccomodation(matchedAc);
-							accomodationStatus.setStatus(null);
-						}
-					}
-					hostListings.add(accomodationStatus);
-				}
+			System.out.println("Matched list size 3");
+
+			String username = user.getUserName();
+			String password = user.getPassword();
+			List<User> userList=UserRepository.findByUserNameAndPassword(username, password);
+			if(userList==null && userList.get(0)==null) {
+				return null;
 			}
+			Long userId = userList.get(0).getUserId();
+			List<Accomodation> accomodationList = AccomodationRepository.findByUsername(username);
+			if(accomodationList==null || accomodationList.get(0)==null) {
+				return null;
+			}
+			Accomodation accomodation = accomodationList.get(0);
+			List<Accomodation> accomodationListMatched = new ArrayList<Accomodation>();
+			if(accomodation != null && accomodation.getUserType()!=null 
+					&& accomodation.getRoommatePreference()!=null && accomodation.getStartDate()!=null) {
+				accomodationListMatched = AccomodationRepository.findByUserTypeAndRoommatePreferenceAndStartDate
+						("host", accomodation.getRoommatePreference(), 
+								accomodation.getStartDate());
+				System.out.println("Matched list size "+accomodationListMatched.size());
+			}
+			for(Accomodation matchedAc: accomodationListMatched) {
+				User student = UserRepository.findByUserName(username);
+				List<StudentHostMatch> match = studentHostMatchRepository.findByUserId(student.getUserId());
+				AccomodationStatus accomodationStatus = new AccomodationStatus();
+				accomodationStatus.setAccomodation(matchedAc);
+				for(StudentHostMatch studentHostMatch: match) {
+					if(matchedAc.getAcId()==studentHostMatch.getAcId()) {
+						accomodationStatus.setStatus(studentHostMatch.getStatus());
+					}
+					else {
+						accomodationStatus.setStatus(null);
+					}
+				}
+				hostListings.add(accomodationStatus);
+			}
+
 		}
 		return hostListings;
 	}
@@ -94,8 +98,8 @@ public class StudentController {
 		}
 		StudentHostMatch studentHostMatch = new StudentHostMatch();
 		studentHostMatch.setStatus("Pending");
-		studentHostMatch.setAcId(accRequested.getAcId());
-		studentHostMatch.setUserId(student.getUserId());
+		studentHostMatch.setAcId(AccomodationRepository.findByUsername(accRequested.getUsername()).get(0).getAcId());
+		studentHostMatch.setUserId(UserRepository.findByUserName(student.getUserName()).getUserId());
 		studentHostMatchRepository.save(studentHostMatch);
 	}
 }
